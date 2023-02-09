5200a3b75c676f77cd9e2f7cfa1445d1835c1de8
Mehmet Fatih Kavum <fkavum@zynga.com>
2023-02-09 19:29:01 +0300

    tutorial4

--- src/Tutorial4/notes.md
+++ src/Tutorial4/notes.md
@@ -10,8 +10,17 @@
 - Vertex Buffer
   -  Its just a buffer. This is a buffer memory in OpenGL so its for our GRam
   -  Concept is: A bunch of data that represent my triangle, im putting it to my VRam, then we are issuing something called drawCall.
-  - Draw call is a draw command, which says, the blob of data you have in you Vram, read and draw it to the screen.
+  - Draw call is a draw command, which says, the blob of data you have in your Vram, read and draw it to the screen.
   - We also need to define how to read and interpret that data and how to put it into screen.
   - after the drawcall to say how to rasterize/draw to screen is defined by shaders!.
 
-- Shader is just a program, runs in the GPU. (4)
+- Shader is just a program, runs in the GPU.
+
+
+- OpenGl operates as a state machine.
+  - You set series of states. like I want you to select this buffer, select this shader then draw.
+  - We are not threat it like a object or so,
+
+
+# DOC
+- doc.gl
--- src/Tutorial4/src/Application.cpp
+++ src/Tutorial4/src/Application.cpp
@@ -26,16 +26,29 @@ int main(void) {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+
+    // We can define vertex buffer, outside the while loop
+    // Inside the while loop we can just say draw the data we define here. we can ofc change it every frame if we like.
+    float positions[6] = {
+            -0.5f, -0.5f,
+            0.0f, 0.5f,
+            0.5f, -0.5f
+    };
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); //Generating buffer and giving us back its id.
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); //This size is in bytes...
+    // gl_static means it will be modified once and used many times. (read the manual)
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f); // Right now, our projection matrix between -1,1...
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+
+        //second arg is basically offset, third arg number of indices to be renderered, (Vertexes in this case.)
+        //What it will draw is, last currently binded buffer with glBindBuffer(GL_ARRAY_BUFFER, buffer);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
